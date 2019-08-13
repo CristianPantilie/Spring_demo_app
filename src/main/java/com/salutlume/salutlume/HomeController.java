c4372d76e1849616541dd272b54aaa21bab7d8ff
Cristian Pantilie <Cristian.Pantilie@teamnet.ro>
2019-08-13 17:48:03 +0300

    s

--- src/main/java/com/salutlume/salutlume/HomeController.java
+++ src/main/java/com/salutlume/salutlume/HomeController.java
@@ -2,6 +2,7 @@ package com.salutlume.salutlume;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
