42f5362e7d1c9e6d02003195dae13df3d33675b9
Cristian Pantilie <Cristian.Pantilie@teamnet.ro>
2019-08-14 11:13:23 +0300

    cc

--- src/main/java/com/salutlume/salutlume/HomeController.java
+++ src/main/java/com/salutlume/salutlume/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class HomeController {
 
     private final GreetingRepository greetingRepo;
